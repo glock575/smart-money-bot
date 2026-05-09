723049405fdf609abfb1b9aa93c14414a570a1cc
glock575 <sergiogoloduk@gmail.com>
2026-05-09 20:55:49 +0300

    Fix string quotes in bot start command reply

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const { Telegraf } = require('telegraf');
 const bot = new Telegraf(process.env.TELEGRAM_BOT_TOKEN);
 
 bot.start((ctx) => {
-  ctx.reply(🚀 Smart Money Bot запущений  Команди: /status /scan);
+  ctx.reply('🚀 Smart Money Bot запущений  Команди: /status /scan');
 });
 
 bot.command('status', (ctx) => {
